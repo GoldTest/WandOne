6ed1b6bc5ba7ac2fd10e1f60d15a38331569991a
试金石 <1304747347@qq.com>
2024-12-04 19:42:05 +0800

    chore 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,33 @@
 import APPViewModel.windowState
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
 import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.DpSize
-import androidx.compose.ui.window.*
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import com.multiplatform.webview.web.LoadingState
 import com.multiplatform.webview.web.WebView
-import com.multiplatform.webview.web.rememberWebViewState
+import com.multiplatform.webview.web.rememberSaveableWebViewState
 import dev.datlag.kcef.KCEF
-import dev.datlag.kcef.KCEFBuilder.Download
-import dev.datlag.kcef.KCEFBuilder.Download.Builder
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import page.tray.Tray
 import java.io.File
 import kotlin.math.max
 
-fun main1() = run {
+fun main() = run {
     application {
         //db
         val dbDirectory = File("data")
@@ -57,74 +56,96 @@ fun main1() = run {
                 app()
             }
         }
+        windowWeb()
     }
 }
 
-fun main() = run {
-    application {
-        Window(onCloseRequest = ::exitApplication) {
-            var restartRequired by remember { mutableStateOf(false) }
-            var downloading by remember { mutableStateOf(0F) }
-            var initialized by remember { mutableStateOf(false) }
-            val download: Download = remember { Builder().github().build() }
-
-            LaunchedEffect(Unit) {
-                withContext(Dispatchers.IO) {
-                    KCEF.init(builder = {
-                        installDir(File("kcef-bundle"))
-                        /*
-                          Add this code when using JDK 17.
-                          Builder().github {
-                              release("jbr-release-17.0.10b1087.23")
-                          }.buffer(download.bufferSize).build()
-                         */
-                        progress {
-                            onDownloading {
-                                downloading = max(it, 0F)
-                            }
-                            onInitialized {
-                                initialized = true
-                            }
+@Composable
+fun windowWeb() {
+    val state = rememberWindowState()
+    state.size = DpSize(600.dp, 900.dp)
+    Window(
+        state = state,
+        onCloseRequest = {},
+        title = "simple"
+    ) {
+        var restartRequired by remember { mutableStateOf(false) }
+        var downloading by remember { mutableStateOf(0F) }
+        var initialized by remember { mutableStateOf(false) }
+        var state by mutableStateOf(rememberSaveableWebViewState("https://music.163.com/outchain/player?type=4&id=526213626&auto=0&height=430"))
+        LaunchedEffect(Unit) {
+            withContext(Dispatchers.IO) {
+                KCEF.init(builder = {
+                    installDir(File("kcef-bundle"))
+                    progress {
+                        onDownloading {
+                            downloading = max(it, 0F)
                         }
-                        settings {
-                            cachePath = File("cache").absolutePath
+                        onInitialized {
+                            initialized = true
                         }
-                    }, onError = {
-                        it?.printStackTrace()
-                    }, onRestartRequired = {
-                        restartRequired = true
-                    })
-                }
+                    }
+                    settings {
+                        cachePath = File("cache").absolutePath
+                    }
+                }, onError = {
+                    it?.printStackTrace()
+                }, onRestartRequired = {
+                    restartRequired = true
+                })
             }
-
-            if (restartRequired) {
-                Text(text = "Restart required.")
-            } else {
-                if (initialized) {
-                    Column(modifier = Modifier.fillMaxSize()) {
-                        val state = rememberWebViewState("https://baidu.com")
-
-                        Text(text = "${state.pageTitle}")
-                        val loadingState = state.loadingState
-                        if (loadingState is LoadingState.Loading) {
-                            LinearProgressIndicator(
-                                progress = loadingState.progress,
-                                modifier = Modifier.fillMaxWidth()
+        }
+        if (restartRequired) {
+            Text(text = "Restart required.")
+        } else {
+            if (initialized) {
+                Column(modifier = Modifier.fillMaxSize()) {
+                    var tab by remember { mutableStateOf(0) }
+                    Row() {
+                        Button(onClick = {
+                            tab = 0
+                        }) {
+                            Text("tab1")
+                        }
+                        Button(onClick = {
+                            tab = 1
+                        }) {
+                            Text("tab2")
+                        }
+                    }
+                    when (tab) {
+                        0 -> Column() {
+                            Text(text = "${state.pageTitle}")
+                            val loadingState = state.loadingState
+                            if (loadingState is LoadingState.Loading) {
+                                LinearProgressIndicator(
+                                    progress = loadingState.progress,
+                                    modifier = Modifier.fillMaxWidth()
+                                )
+                            }
+                            WebView(
+                                state = state,
+                                modifier = Modifier.fillMaxSize(),
+                                onCreated = { webViewState ->
+                                    println("created")
+                                },
+                                onDispose = { webViewState ->
+                                    println("disposed")
+                                }
                             )
                         }
-                        WebView(
-                            state, modifier = Modifier.fillMaxSize()
-                        )
-                    }
-                } else {
-                    Text(text = "Downloading $downloading%")
-                }
-            }
 
-            DisposableEffect(Unit) {
-                onDispose {
-                    KCEF.disposeBlocking()
+                        1 -> Text("good")
+                    }
                 }
+            } else {
+                Text(text = "Downloading $downloading%")
+            }
+        }
+
+        DisposableEffect(Unit) {
+            onDispose {
+                KCEF.disposeBlocking()
             }
         }
     }
